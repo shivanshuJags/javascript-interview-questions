b830be6cb73155b1f4ce24acaeff2cc51debc50b
shivanshuJags <shivanshusahu899@gmail.com>
2024-06-13 15:10:18 +0530

    webpack setup

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "../styles/styles.css";
+import "../exercise/script1.js";
+import "../exercise/script2.js";
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: "./dist", // Update contentBase to static
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // If you have an HTML file
+    }),
+  ],
+};
